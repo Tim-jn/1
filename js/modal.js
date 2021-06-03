2f3cdd9702d849edce8a0a32b1bcc8e0c8da1a7d
Tim <Tim280696@hotmail.com>
2021-06-03 11:49:18 +0200

    modifications CSS, première piste ajout form validation JS

--- js/modal.js
+++ js/modal.js
@@ -24,3 +24,32 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+// form validation
+document.forms["reserve"].addEventListener("submit", function(e) {
+  
+	var erreur;
+	var inputs = this;
+ 
+	/* Traitement cas par cas (input unique)
+	  if (inputs["email"].value != "") {
+		erreur = "Adresse email incorrecte";
+	}*/
+ 
+	// Traitement générique
+	for (var i = 0; i < inputs.length; i++) {
+		console.log(inputs[i]);
+		if (!inputs[i].value) {
+			erreur = "Veuillez renseigner tous les champs";
+			break;
+		}
+	}
+
+	if (erreur) {
+		e.preventDefault();
+		document.getElementById("erreur").innerHTML = erreur;
+		return false;
+	} else {
+		alert('Formulaire envoyé !');
+	}
+});
