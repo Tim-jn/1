ba9e0be0abbf568e23d2d1e7e96a69331e068c46
Tim <Tim280696@hotmail.com>
2021-06-04 14:11:43 +0200

    ajout package.json, navbarre adaptée responsive, suppression form validation (test nouvelle méthode)

--- js/modal.js
+++ js/modal.js
@@ -26,30 +26,9 @@ function closeModal() {
 }
 
 // form validation
-document.forms["reserve"].addEventListener("submit", function(e) {
-  
-	var erreur;
-	var inputs = this;
- 
-	/* Traitement cas par cas (input unique)
-	  if (inputs["email"].value != "") {
-		erreur = "Adresse email incorrecte";
-	}*/
- 
-	// Traitement générique
-	for (var i = 0; i < inputs.length; i++) {
-		console.log(inputs[i]);
-		if (!inputs[i].value) {
-			erreur = "Veuillez renseigner tous les champs";
-			break;
-		}
-	}
 
-	if (erreur) {
-		e.preventDefault();
-		document.getElementById("erreur").innerHTML = erreur;
-		return false;
-	} else {
-		alert('Formulaire envoyé !');
-	}
-});
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
