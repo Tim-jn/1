1cc2ea200a66ba516b2dc5279dee695afe832dec
Tim <Tim280696@hotmail.com>
2021-06-16 14:04:45 +0200

    validation form opérationnelle, ajout de prettier, changements CSS

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
+// top navigation display for responsive
 function editNav() {
-  var topNav = document.getElementById("myTopnav");
-  if (topNav.className === "topnav") {
-    topNav.className += " responsive";
+  var topNavDisplay = document.getElementById("myTopnav");
+  if (topNavDisplay.className === "topnav") {
+    topNavDisplay.className += " responsive";
   } else {
-    topNav.className = "topnav";
+    topNavDisplay.className = "topnav";
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+// launch,close modal elements
+const formModalBg = document.querySelector("#form-modal.bground");
+const formModalBtn = document.querySelectorAll(".btn-signup");
+const confirmationModalBg = document.querySelector("#confirmation-modal.bground");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+formModalBtn.forEach((btn) => btn.addEventListener("click", launchFormModal));
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+function launchFormModal() {
+  formModalBg.style.display = "block";
+}
+function launchConfirmationModal() {
+  confirmationModalBg.style.display = "block";
 }
 
 // close modal form
-function closeModal() {
-  modalbg.style.display = "none";
+function closeFormModal() {
+  formModalBg.style.display = "none";
+}
+function closeConfirmationModal() {
+  confirmationModalBg.style.display = "none";
+  formModalBg.style.display = "none";
 }
 
 // form validation
@@ -32,85 +41,84 @@ const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const checkbox1 = document.getElementById("checkbox1");
-const allFields = [first, last, email, birthdate, quantity, checkbox1];
+const errors = [];
+const error = "";
 
-reserve.addEventListener("submit", e => {
-	e.preventDefault();
-	// Vérification des champs
-	validate();
+reserve.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // Vérification des champs
+  if (validate()) {
+    launchConfirmationModal();
+  } else {
+    errors.splice(0, errors.length); // reset la liste d'erreurs
+  }
 });
 
-function validate() {
-	// On reprend la valeur de chaque variable et on enlève les espaces avec trim()
-	const firstValue = first.value.trim();
-	const lastValue = last.value.trim();
-	const emailValue = email.value.trim();
-	const birthdateValue = birthdate.value.trim();
-	const quantityValue = quantity.value.trim();
-
-	if (firstValue === "") {
-		setErrorFor(first, "*Veuillez renseigner un prénom");
-	} else if (firstValue.length <= 2) {
-		setErrorFor(first, "*Veuillez renseigner un prénom d'au moins deux caractères");
-	} else {
-		setSuccessFor(first);
-	}
-
-	if (lastValue === "") {
-		setErrorFor(last, "*Veuillez renseigner un nom");
-	} else if (lastValue.length <= 2) {
-		setErrorFor(last, "*Veuillez renseigner un nom d'au moins deux caractères");
-	} else {
-		setSuccessFor(last);
-	}
-
-	if (emailValue === "") {
-		setErrorFor(email, "*Veuillez renseigner un email");
-	} else {
-		setSuccessFor(email);
-	}
-
-	if (birthdateValue === "") {
-		setErrorFor(birthdate, "*Veuillez renseigner une date de naissance");
-	} else {
-		setSuccessFor(birthdate);
-	}
-
-	if (quantityValue === "") {
-		setErrorFor(quantity, "*Veuillez renseigner un nombre");
-	} else {
-		setSuccessFor(quantity);
-	}
-	
-	if (checkbox1.checked === false) {
-		setErrorFor(checkbox1, "*Cette case est obligatoire");
-	} else {
-		setSuccessFor(checkbox1);
-	}
-}
-
-/** Set error message on target input
- * 
- * @param {*} input 
- * @param {string} message 
+/**
+ * @return {boolean}
  */
-function setErrorFor(input, message) {
-	const formData = input.parentElement; 					// sélectionne l'élément parent de input
-	const isValid = formData.querySelector(".isValid");	    // sélectionne la div warning de la class formData
-	formData.className = "formData error";					// Crée une class formData.error
-	isValid.innerText = message;							// Affiche le message d'erreur propre à chaque erreur						
-}
-/** Set `success` class on target input  */
-function setSuccessFor(input) {	
-	const formData = input.parentElement;					// sélectionne l'élément parent de input
-	formData.className = "formData succes";					// Crée une class formData.succes		
+function validate() {
+  // On reprend la valeur de chaque variable et on enlève les espaces avec trim()
+  const firstValue = first.value.trim();
+  const lastValue = last.value.trim();
+  const emailValue = email.value.trim();
+  const birthdateValue = birthdate.value.trim();
+  const quantityValue = quantity.value.trim();
+
+  if (firstValue === "") {
+    setErrorFor(first, "*Veuillez renseigner un prénom d'au moins deux caractères");
+    errors.push(error); // ajoute un "error" au tableau "errors"
+  } else {
+    setSuccessFor(first);
+  }
+
+  if (lastValue === "") {
+    setErrorFor(last, "*Veuillez renseigner un nom d'au moins deux caractères");
+    errors.push(error);
+  } else {
+    setSuccessFor(last);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "*Veuillez renseigner un email");
+    errors.push(error);
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (birthdateValue === "") {
+    setErrorFor(birthdate, "*Veuillez renseigner une date de naissance");
+    errors.push(error);
+  } else {
+    setSuccessFor(birthdate);
+  }
+
+  if (quantityValue === "") {
+    setErrorFor(quantity, "*Veuillez renseigner un nombre");
+    errors.push(error);
+  } else {
+    setSuccessFor(quantity);
+  }
+
+  if (checkbox1.checked === false) {
+    setErrorFor(checkbox1, "*Cette case est obligatoire");
+    errors.push(error);
+  } else {
+    setSuccessFor(checkbox1);
+  }
+
+  return errors.length === 0; // compte le nombre d'erreurs, si = 0 valide, sinon erreur
 }
 
-function submitForm() {
-	if (/* valide */ true) {
-		allFields.forEach(setSuccessFor)
-		// openConfirmationModal();
-	} else {
-		alert("tous les éléments doivent être valide");
-	}
+// Affiche un message d'erreur sur l'input concerné
+function setErrorFor(input, message) {
+  const formData = input.parentElement; // sélectionne l'élément parent de input
+  const isValid = formData.querySelector(".isValid"); // sélectionne la div warning de la class formData
+  formData.className = "formData error"; // Crée une class formData.error
+  isValid.innerText = message; // Affiche le message d'erreur propre à chaque erreur
+}
+// Affiche une classe succes sur l'input concerné
+function setSuccessFor(input) {
+  const formData = input.parentElement; // sélectionne l'élément parent de input
+  formData.className = "formData succes"; // Crée une class formData.succes
 }
